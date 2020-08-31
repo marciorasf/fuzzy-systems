950b9af90c641a0cf0baef1b68400aafd5626aea
Marcio Souza Filho <marciorasf@gmail.com>
2020-08-31 00:56:19 -0300

    organize script in functions

--- tp1/fuzzy_mountain.py
+++ tp1/fuzzy_mountain.py
@@ -2,66 +2,87 @@
 from scipy.io import loadmat
 import pandas as pd
 import numpy as np
-from c_means import euclNorm
 from functools import reduce
 import plotly.express as px
 
-# %% initialize input data
-nPoints = 100
-xDimension = 2
+
+# %% declare functions
+def fuzzyMountain(data):
+    xColumns = data.columns
+
+    centroids = pd.DataFrame(columns=xColumns)
+
+    densities = calculateDensities(data, 1)
+
+    maxDensityPoint, maxDensity = getMaxDensityPointAndValue(densities)
+
+    centroids = centroids.append(maxDensityPoint)
+
+    densities = updateDensities(
+        data, densities, maxDensityPoint, maxDensity, 1.5)
+
+    maxDensityPoint, maxDensity = getMaxDensityPointAndValue(densities)
+
+    centroids = centroids.append(maxDensityPoint)
+
+    return centroids
+
+
+def calculateDensities(data, radius):
+    denominator = (radius/2)**2
+
+    densities = pd.DataFrame(
+        np.zeros((nPoints, 1)),
+        columns=["density"]
+    )
+
+    for row_label, row in data.iterrows():
+        density = reduce(
+            lambda sum, x: sum +
+            np.exp(- ((euclNorm(row - x)**2)/denominator)),
+            data.loc[:, :].to_numpy(),
+            0
+        )
+        densities.loc[row_label, "density"] = density
+
+    return densities
+
+
+def getMaxDensityPointAndValue(densities):
+    maxDensityIndex = densities["density"].idxmax()
+
+    maxDensity = densities.loc[maxDensityIndex, "density"]
+    point = data.iloc[maxDensityIndex]
+
+    return [point, maxDensity]
+
+
+def updateDensities(data, densities, maxDensityPoint, maxDensity, radius):
+    denominator = (radius/2)**2
+
+    for row_label, row in data.iterrows():
+        density = np.exp(- ((euclNorm(row - maxDensityPoint)**2)/denominator))
+        densities.loc[row_label, "density"] -= maxDensity*density
+
+    return densities
+
+
+def formatData(data):
+    xDimension = data.shape[1]
+    xColumns = [f'x{i}' for i in range(xDimension)]
+    data = pd.DataFrame(rawData, columns=xColumns)
+    return data
+
+
+def euclNorm(arr):
+    return np.linalg.norm(arr, ord=2)
+
+
+# %% main script
+nPoints = 10
+xDimension = 10
 rawData = np.random.normal(0.5, 0.4, (nPoints, xDimension))
 
-xColumns = [f'x{i}' for i in range(xDimension)]
-df = pd.DataFrame(rawData, columns=xColumns)
+data = formatData(rawData)
 
-limitsColumns = ["min", "max"]
-limits = pd.DataFrame([], columns=limitsColumns)
-for col in xColumns:
-    limits.loc[col, :] = [df[col].min(), df[col].max()]
-
-fig = px.scatter(df, x="x0", y="x1")
-fig.show()
-
-# %% initialize algorithm data
-
-centroids = pd.DataFrame(columns=xColumns)
-densities = pd.DataFrame(
-    np.zeros((nPoints, 1)),
-    columns=["density"]
-)
-
-
-# %% step 1
-radiusA = 1
-denominatorA = (radiusA/2)**2
-
-for row_label, row in df.iterrows():
-    density = reduce(
-        lambda sum, x: sum + np.exp(- ((euclNorm(row - x)**2)/denominatorA)),
-        df.loc[:, :].to_numpy(),
-        0
-    )
-    densities.loc[row_label, "density"] = density
-
-maxDensityIndex = densities["density"].idxmax()
-maxDensity =  densities.loc[maxDensityIndex, "density"]
-currentSelectedCentroid = df.iloc[maxDensityIndex]
-centroids = centroids.append(currentSelectedCentroid)
-
-# %% step 2
-radiusB = 1.5*radiusA
-denominatorB = (radiusB/2)**2
-
-for row_label, row in df.iterrows():
-    density = np.exp(- ((euclNorm(row - currentSelectedCentroid)**2)/denominatorB))
-    densities.loc[row_label, "density"] -= maxDensity*density
-
-
-maxDensityIndex = densities["density"].idxmax()
-maxDensity =  densities.iloc[maxDensityIndex]
-currentSelectedCentroid = df.iloc[maxDensityIndex]
-centroids = centroids.append(currentSelectedCentroid)
-
-fig = px.scatter(centroids, x="x0", y="x1")
-fig.show()
-# %%
+centroids = fuzzyMountain(data)
